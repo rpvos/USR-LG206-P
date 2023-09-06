52f6fe81596fc387ba8318d65c533e465509f791
Rik <rik.vos01@gmail.com>
2023-09-06 11:27:42 +0200

    [Add] Settings as struct with factory settings and added method skeleton

--- include/USR-LR206-P.h
+++ include/USR-LR206-P.h
@@ -1,8 +1,28 @@
-#ifndef USRLR206P_H
-#define USRLR206P_H
+/**
+ * @file USR-LR206-P.cpp
+ * @author Rik Vos (rik.vos01@gmail.com)
+ * @brief Library for using USR-LR206-P
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef USRLR206P_H_
+#define USRLR206P_H_
 
 #include <Arduino.h>
 
+/**
+ * @brief OUT is used as a identifier for output parameters
+ *
+ */
+#define OUT
+/**
+ * @brief SETTING_UNDEFINED is used as a identifier for not yet retrieved or set settings
+ *
+ */
 #define SETTING_UNDEFINED -1
 
 // All settings for workmode
@@ -25,7 +45,7 @@
 #define PARITY_ODD 3
 
 // All settings for flowcontrol
-#define FLOWCONTROL_485 1
+#define FLOWCONTROL_485 1 // 485 Flow Control
 #define FLOWCONTROL_NFC 2 // No Flow Control
 
 // All settings for power consumption mode
@@ -44,90 +64,99 @@
 #define LoRaAirRateLevel_10937 9
 #define LoRaAirRateLevel_21875 10
 
-// struct USR_LG_206_P_SETTINGS_DEFAULT
-// {
-//     bool ATMode = false;
-//     bool commandEchoFunction = false;
-//     int nodeID = -1;
-//     String firmwareVersion = "";
-//     char workMode = WMODE_TRANS;
-//     USR_LG_206_P_UART_SETTINGS uart;
-//     char powerMode = POWERMODE_RUN;
-//     int wakingUpInterval = 2000;
-//     char loraAirRateLevel = LoRaAirRateLevel_21875;
-//     unsigned int destinationAddress = 0;
-//     char channel = 65;
-//     bool forwardErrorCorrection = false;
-//     char transmittingPower = 20;
-//     char key[16]; // 16 bytes HEX format character string
-// };
+class USR_LG_206_P_UART_SETTINGS
+{
+    char buadrate = BAUDRATE_115200;
+    char dataBits = 8;
+    char stopBits = 1;
+    char parity = PARITY_NONE;
+    char flowControl = FLOWCONTROL_485;
+};
 
+typedef struct
+{
+    int ATMode;
+    int commandEchoFunction;
+    int nodeID;
+    String firmwareVersion;
+    int workMode;
+    USR_LG_206_P_UART_SETTINGS *uart;
+    int powerMode;
+    int wakingUpInterval;
+    int loraAirRateLevel;
+    unsigned int destinationAddress;
+    int channel;
+    int forwardErrorCorrection;
+    int transmittingPower;
+    char key[16]; // 16 bytes HEX format character string
+} USR_LG_206_P_SETTINGS_t;
+
+USR_LG_206_P_SETTINGS_t factory_settings = {
+    .ATMode = false,
+    .commandEchoFunction = false,
+    .nodeID = -1,
+    .firmwareVersion = "",
+    .workMode = WMODE_TRANS,
+    .uart = new USR_LG_206_P_UART_SETTINGS(),
+    .powerMode = POWERMODE_RUN,
+    .wakingUpInterval = 2000,
+    .loraAirRateLevel = LoRaAirRateLevel_21875,
+    .destinationAddress = 0,
+    .channel = 65,
+    .forwardErrorCorrection = false,
+    .transmittingPower = 20,
+    // .key = {}
+};
+
+USR_LG_206_P_SETTINGS_t undefined_settings = {
+    .ATMode = false,
+    .commandEchoFunction = SETTING_UNDEFINED,
+    .nodeID = SETTING_UNDEFINED,
+    .firmwareVersion = "",
+    .workMode = SETTING_UNDEFINED,
+    .uart = 0,
+    .powerMode = SETTING_UNDEFINED,
+    .wakingUpInterval = SETTING_UNDEFINED,
+    .loraAirRateLevel = SETTING_UNDEFINED,
+    .destinationAddress = 0,
+    .channel = SETTING_UNDEFINED,
+    .forwardErrorCorrection = SETTING_UNDEFINED,
+    .transmittingPower = SETTING_UNDEFINED,
+    // .key[16] = {}
+};
+
+/**
+ * @brief Class used to connect and communicate to a LoRa module type USR_LG_206_P
+ *
+ */
 class USR_LG_206_P
 {
 public:
-    /// @brief Class used to store the settings of the UART of the LoRa module
-    class USR_LG_206_P_UART_SETTINGS
-    {
-    public:
-        char buadrate = BAUDRATE_115200;
-        char dataBits = 8;
-        char stopBits = 1;
-        char parity = PARITY_NONE;
-        char flowControl = FLOWCONTROL_485;
-    };
-
-    /// @brief Class used to store the settings of the LoRa module
-    class USR_LG_206_P_SETTINGS
-    {
-    public:
-        int ATMode = false;
-        int commandEchoFunction = SETTING_UNDEFINED;
-        int nodeID = SETTING_UNDEFINED;
-        String firmwareVersion = "";
-        int workMode = SETTING_UNDEFINED;
-        USR_LG_206_P_UART_SETTINGS *uart;
-        int powerMode = SETTING_UNDEFINED;
-        int wakingUpInterval = SETTING_UNDEFINED;
-        int loraAirRateLevel = SETTING_UNDEFINED;
-        unsigned int destinationAddress = SETTING_UNDEFINED;
-        int channel = SETTING_UNDEFINED;
-        bool forwardErrorCorrection = SETTING_UNDEFINED;
-        int transmittingPower = SETTING_UNDEFINED;
-        char key[16]; // 16 bytes HEX format character string
-    };
-
-private:
-    Stream *serial;
-    USR_LG_206_P_SETTINGS *settings;
-
-    /// @brief Function used to set the value on the LoRa module
-    /// @param command which is used to set the value
-    /// @return true of succesfull, false if unsuccesfull
-    int set_command(String command);
-
-    /// @brief Function used to get a value from the LoRa module
-    /// @param command Command for wich the value is stored
-    /// @return String containing this value
-    String get_command(String command);
-
-public:
+    /**
+     * @brief Construct a new usr lg 206 p object
+     *
+     * @param serial the stream to which data needs to be sent to communicate with the module
+     */
     USR_LG_206_P(Stream *serial);
 
     // TODO
-    int retrieve_settings();
+    int retrieve_settings(void);
 
     // TODO ADD reset to factory settings
 
     // TODO
     //  int set_settings(USR_LG_206_P_SETTINGS* settings);
 
-    /// @brief Function to start the AT mode
-    /// @return true if succesfull and false if unsuccesfull
-    int begin_AT_mode();
+    /**
+     * @brief Function to start the AT mode
+     *
+     * @return int, true if succesfull and false if unsuccesfull
+     */
+    int begin_AT_mode(void);
 
     /// @brief Function to end the AT mode and start transmission mode
     /// @return true if succesfull and false if unsuccesfull
-    int end_AT_mode();
+    int end_AT_mode(void);
 
     /// @brief Function used to set the function echo boolean
     /// @param isOn the value to which it is set
@@ -135,42 +164,53 @@ public:
     int set_echo(bool isOn);
 
     /// @brief Function used to get the currect value of the echo function command on the LoRa module
-    /// @return The value of the echo command or -1 if not succeeded
-    int get_echo();
+    /// @param isOn OUTPUT variable used to store the requested value
+    /// @return true of succesfull, false if unsuccesfull
+    int get_echo(OUT bool &isOn);
 
     /// @brief Function used to restart the LoRa module
     /// @return true if succesfull
-    int restart();
+    int restart(void);
 
     /// @brief Function used to set the current settings as new default
     /// @return true of succesfull, false if unsuccesfull
-    int save_as_default();
+    int save_as_default(void);
 
     // TODO
-    // int reset_to_default();
+    int reset_to_default(void);
+
     // TODO
-    // int get_node_id();
+    int get_node_id(void);
     // TODO
-    // int get_firmware_version();
+    int get_firmware_version(void);
 
     /// @brief Function used to set the workmode
     /// @param wmode workmode (WMODE_TRANS = 1, WMODE_FP = 2)
     /// @return true of succesfull, false if unsuccesfull
     int set_wmode(int wmode);
+
+    /// @brief Function used to get the work mode
+    /// @param wmode OUTPUT variable used to store the requested value
+    /// @return true of succesfull, false if unsuccesfull
+    int get_wmode(OUT int &wmode);
+
     // TODO
-    // int get_wmode();
+    int set_uart(USR_LG_206_P_UART_SETTINGS *settings);
+
     // TODO
-    // int set_uart();
+    /// @brief Function used to get the uart settings
+    /// @param settings OUTPUT variable used to store the requested value
+    /// @return true of succesfull, false if unsuccesfull
+    int get_uart(OUT USR_LG_206_P_UART_SETTINGS &settings);
+
     // TODO
-    // int get_uart();
+    int set_power_consumption_mode(void);
     // TODO
-    // int set_power_consumption_mode();
+    int get_power_consumption_mode(void);
     // TODO
-    // int get_power_consumption_mode();
+    int set_waking_up_interval(void);
     // TODO
-    // int get_waking_up_interval();
-    // TODO
-    // int set_waking_up_interval();
+    int get_waking_up_interval(void);
 
     /// @brief Function used to set the LoRa air rate level
     /// @param speed value for air rate 1-10 as following values
@@ -188,7 +228,7 @@ public:
     int set_speed(int speed);
 
     // TODO
-    // int get_speed();
+    int get_speed(void);
 
     /// @brief Function used to set the destination address
     /// @param address value between 0-65535
@@ -196,7 +236,7 @@ public:
     int set_address(int address);
 
     // TODO
-    // int get_address();
+    // int get_address(void);
 
     /// @brief Function used to set the LoRa air rate level
     /// @param channel value for channel is as following values in the following ranges
@@ -206,30 +246,59 @@ public:
     int set_channel(int channel);
 
     // TODO
-    // int get_channel();
+    // int get_channel(void);
     // TODO
-    // int set_forward_error_correction();
+    // int set_forward_error_correction(void);
     // TODO
-    // int get_forward_error_correction();
+    // int get_forward_error_correction(void);
     // TODO
-    // int set_power_transmission_mode();
+    // int set_power_transmission_mode(void);
     // TODO
-    // int get_power_transmission_mode();
+    // int get_power_transmission_mode(void);
     // TODO
-    // int set_transmission_interval();
+    // int set_transmission_interval(void);
     // TODO
-    // int get_transmission_interval();
+    // int get_transmission_interval(void);
     // TODO
-    // int set_key();
+    // int set_key(void);
 
     /// @brief Function used to retrieve a message from the module
     /// @return the data received
-    String retrieve_message();
+    String retrieve_message(void);
 
     /// @brief Function used to send data
     /// @param message data that needs to be send
     /// @return amount of bytes written
     int send_message(char *message);
+
+private:
+    /**
+     * @brief Stream to which the communication with the module is sent
+     *
+     */
+    Stream *serial;
+
+    /**
+     * @brief Object to keep the settings of the LoRa module
+     * This is to counter frequent calls
+     */
+    USR_LG_206_P_SETTINGS_t *settings;
+
+    /**
+     * @brief Function used to set the value on the LoRa module
+     *
+     * @param command which is used to set the value
+     * @return int, true if succesfull, false if unsuccesfull
+     */
+    int set_command(String command);
+
+    /**
+     * @brief Function used to get a setting from the LoRa module
+     *
+     * @param command Command for wich the value is stored
+     * @return String containing this value
+     */
+    String get_command(String command);
 };
 
-#endif
+#endif // USRLR206P_H_
--- src/USR-LR206-P.cpp
+++ src/USR-LR206-P.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file USR-LR206-P.cpp
+ * @author Rik Vos (rik.vos01@gmail.com)
+ * @brief Library for using USR-LR206-P
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include "USR-LR206-P.h"
 #include <Arduino.h>
 #include <ArduinoLog.h>
@@ -5,80 +16,28 @@
 USR_LG_206_P::USR_LG_206_P(Stream *serial)
 {
     this->serial = serial;
-    this->settings = new USR_LG_206_P_SETTINGS();
-};
-
-int USR_LG_206_P::retrieve_settings()
-{
-    USR_LG_206_P_SETTINGS settings;
-    settings.ATMode = true;
-
-    settings.commandEchoFunction = get_echo();
-    // TODO add other settings
-
+    USR_LG_206_P_SETTINGS_t settings = undefined_settings;
     this->settings = &settings;
 };
 
-/// @brief Function used to set the value on the LoRa module
-/// @param command which is used to set the value
-/// @return true of succesfull, false if unsuccesfull
-int USR_LG_206_P::set_command(String command)
+int USR_LG_206_P::retrieve_settings(void)
 {
-    serial->println("AT" + command);
-    String expected = command;
-    String data = serial->readString();
-    if (data != expected)
-    {
-        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
-        return false;
-    }
+    USR_LG_206_P_SETTINGS_t settings;
+    settings.ATMode = true;
 
-    data = serial->readString();
-    expected = "OK";
-    if (data != expected)
+    bool isOn;
+    if (get_echo(isOn))
     {
-        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
-        return false;
+        settings.commandEchoFunction = isOn;
     }
+    // TODO add other settings
+
+    this->settings = &settings;
 
     return true;
 };
 
-/// @brief Function used to get a value from the LoRa module
-/// @param command Command for wich the value is stored
-/// @return String containing this value
-String USR_LG_206_P::get_command(String command)
-{
-    serial->println("AT" + command);
-    String expected = command;
-    String data = serial->readString();
-
-    if (data != expected)
-    {
-        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
-        return "";
-    }
-
-    data = serial->readString();
-
-    expected = command + ":";
-
-    String subStringed = data.substring(0, data.indexOf(':'));
-
-    if (subStringed != expected)
-    {
-        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
-        return "";
-    }
-
-    String value = data.substring(data.indexOf(':'), data.indexOf("OK"));
-
-    return value;
-};
-
-/// @brief Function to start the AT mode
-/// @return true if succesfull and false if unsuccesfull
-int USR_LG_206_P::begin_AT_mode()
+int USR_LG_206_P::begin_AT_mode(void)
 {
     // Check if the LoRa module is already in AT mode
     if (settings->ATMode)
@@ -114,9 +73,7 @@ int USR_LG_206_P::begin_AT_mode()
     return true;
 };
 
-/// @brief Function to end the AT mode and start transmission mode
-/// @return true if succesfull and false if unsuccesfull
-int USR_LG_206_P::end_AT_mode()
+int USR_LG_206_P::end_AT_mode(void)
 {
     // Check if LoRa module was already out of AT mode
     if (!settings->ATMode)
@@ -137,11 +94,8 @@ int USR_LG_206_P::end_AT_mode()
         settings->ATMode = false;
         return true;
     }
-}
+};
 
-/// @brief Function used to set the function echo boolean
-/// @param isOn the value to which it is set
-/// @return true of succesfull, false if unsuccesfull
 int USR_LG_206_P::set_echo(bool isOn)
 {
     String command = "+E=";
@@ -166,10 +120,7 @@ int USR_LG_206_P::set_echo(bool isOn)
     return response;
 };
 
-/// @brief Function used to get the currect value of the echo function command on the LoRa module,
-/// this can't be done using the get_command because the returned string is unconventional
-/// @return The value of the echo command or -1 if not succeeded
-int USR_LG_206_P::get_echo()
+int USR_LG_206_P::get_echo(OUT bool &isOn)
 {
     if (settings->commandEchoFunction != SETTING_UNDEFINED)
     {
@@ -185,7 +136,7 @@ int USR_LG_206_P::get_echo()
     if (data != expected)
     {
         Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
-        return -1;
+        return false;
     }
 
     data = serial->readString();
@@ -197,27 +148,27 @@ int USR_LG_206_P::get_echo()
     if (subStringed != expected)
     {
         Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), subStringed.c_str());
-        return -1;
+        return false;
     }
 
     String value = data.substring(data.indexOf('='));
     if (value == "ON")
     {
         settings->commandEchoFunction = true;
+        isOn = true;
         return true;
     }
     else if (value == "OFF")
     {
         settings->commandEchoFunction = false;
-        return false;
+        isOn = false;
+        return true;
     }
 
-    return -1;
+    return false;
 };
 
-/// @brief Function used to restart the LoRa module
-/// @return true of succesfull, false if unsuccesfull
-int USR_LG_206_P::restart()
+int USR_LG_206_P::restart(void)
 {
     String command = "AT+Z";
     serial->println(command);
@@ -243,9 +194,7 @@ int USR_LG_206_P::restart()
     return true;
 };
 
-/// @brief Function used to set the current settings as new default
-/// @return true of succesfull, false if unsuccesfull
-int USR_LG_206_P::save_as_default()
+int USR_LG_206_P::save_as_default(void)
 {
     String command = "AT+CFGTF";
     serial->println(command);
@@ -271,9 +220,40 @@ int USR_LG_206_P::save_as_default()
     return true;
 };
 
-/// @brief Function used to set the workmode
-/// @param wmode workmode (WMODE_TRANS = 1, WMODE_FP = 2)
-/// @return true of succesfull, false if unsuccesfull
+int USR_LG_206_P::reset_to_default(void)
+{
+    String command = "AT+RELD";
+    serial->println(command);
+    String response = serial->readString();
+    String expected = command;
+
+    if (response != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), response.c_str());
+        return false;
+    }
+
+    response = serial->readString();
+
+    expected = "REBOOTING";
+
+    if (response != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), response.c_str());
+        return false;
+    }
+
+    return true;
+};
+
+int USR_LG_206_P::get_node_id(void){
+
+};
+
+int USR_LG_206_P::get_firmware_version(void){
+
+};
+
 int USR_LG_206_P::set_wmode(int wmode = WMODE_TRANS)
 {
     String command = "+WMODE=";
@@ -303,22 +283,50 @@ int USR_LG_206_P::set_wmode(int wmode = WMODE_TRANS)
     return response;
 };
 
-/// @brief Function used to set the LoRa air rate level
-/// @param speed value for air rate 1-10 as following values
-/// 1: 268bps
-/// 2: 488bps
-/// 3: 537bps
-/// 4: 878bps
-/// 5: 977bps
-/// 6: 1758bps
-/// 7: 3125bps
-/// 8: 6250bps
-/// 9: 10937bps
-/// 10: 21875bp
-/// @return true of succesfull, false if unsuccesfull
+int USR_LG_206_P::get_wmode(OUT int &wmode)
+{
+    if (settings->workMode != SETTING_UNDEFINED)
+    {
+        return settings->workMode;
+    }
+
+    String command = "+WMODE";
+    String value = get_command(command);
+
+    if (value == "TRANS")
+    {
+        settings->workMode = WMODE_TRANS;
+        wmode = WMODE_TRANS;
+        return true;
+    }
+    else if (value == "FP")
+    {
+        settings->workMode = WMODE_FP;
+        wmode = WMODE_TRANS;
+        return true;
+    }
+
+    return false;
+};
+
+int USR_LG_206_P::set_uart(USR_LG_206_P_UART_SETTINGS *settings){
+
+};
+
+int USR_LG_206_P::get_uart(OUT USR_LG_206_P_UART_SETTINGS &settings){
+
+};
+
+int USR_LG_206_P::set_power_consumption_mode(void){};
+
+int USR_LG_206_P::get_power_consumption_mode(void){};
+
+int USR_LG_206_P::set_waking_up_interval(void){};
+int USR_LG_206_P::get_waking_up_interval(void){};
+
 int USR_LG_206_P::set_speed(int speed = 10)
 {
-    if (1 <= speed <= 10)
+    if (1 <= speed && speed <= 10)
     {
         String command = "+SPD=" + speed;
 
@@ -335,14 +343,13 @@ int USR_LG_206_P::set_speed(int speed = 10)
     }
 
     return false;
-}
+};
 
-/// @brief Function used to set the destination address
-/// @param address value between 0-65535
-/// @return true of succesfull, false if unsuccesfull
+int USR_LG_206_P::get_speed(void){};
+
 int USR_LG_206_P::set_address(int address = 0)
 {
-    if (0 <= address <= 65535)
+    if (0 <= address && address <= 65535)
     {
         String command = "+ADDR=" + address;
 
@@ -359,16 +366,11 @@ int USR_LG_206_P::set_address(int address = 0)
     }
 
     return false;
-}
+};
 
-/// @brief Function used to set the LoRa air rate level
-/// @param channel value for channel is as following values in the following ranges
-/// L: 0~127 Working frequency band=(398+ch)MHz (Default is 72, frequency band 470Mhz)
-/// H: 0~127 Working frequency band=(803+ch)Mhz (Default is 65, frequency band 868Mhz)
-/// @return true of succesfull, false if unsuccesfull
 int USR_LG_206_P::set_channel(int channel = 65)
 {
-    if (0 <= channel <= 127)
+    if (0 <= channel && channel <= 127)
     {
         String command = "+CH=" + channel;
 
@@ -385,11 +387,9 @@ int USR_LG_206_P::set_channel(int channel = 65)
     }
 
     return false;
-}
+};
 
-/// @brief Function used to retrieve a message from the module
-/// @return the data received
-String USR_LG_206_P::retrieve_message()
+String USR_LG_206_P::retrieve_message(void)
 {
     if (serial->available())
     {
@@ -399,9 +399,6 @@ String USR_LG_206_P::retrieve_message()
     return "";
 };
 
-/// @brief Function used to send data
-/// @param message data that needs to be send
-/// @return amount of bytes written
 int USR_LG_206_P::send_message(char *message)
 {
 
@@ -412,4 +409,55 @@ int USR_LG_206_P::send_message(char *message)
     }
 
     return false;
-}
+};
+
+int USR_LG_206_P::set_command(String command)
+{
+    serial->println("AT" + command);
+    String expected = command;
+    String data = serial->readString();
+    if (data != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
+        return false;
+    }
+
+    data = serial->readString();
+    expected = "OK";
+    if (data != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
+        return false;
+    }
+
+    return true;
+};
+
+String USR_LG_206_P::get_command(String command)
+{
+    serial->println("AT" + command);
+    String expected = command;
+    String data = serial->readString();
+
+    if (data != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
+        return "";
+    }
+
+    data = serial->readString();
+
+    expected = command + ":";
+
+    String subStringed = data.substring(0, data.indexOf(':'));
+
+    if (subStringed != expected)
+    {
+        Log.warningln("Received data is not \"%s\" but \"%s\"", expected.c_str(), data.c_str());
+        return "";
+    }
+
+    String value = data.substring(data.indexOf(':'), data.indexOf("OK"));
+
+    return value;
+};
